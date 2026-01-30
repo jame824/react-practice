ff693e2a13c99de8f4bf616b57919297718bc569
James Fu <fu.jinghua8@gmail.com>
2026-01-30 12:10:59 -0800

    feat: implemented modal for actions and rename action functionality

--- storage-app/components/ActionDropdown.tsx
+++ storage-app/components/ActionDropdown.tsx
@@ -1,6 +1,14 @@
 "use client";
 
-import { Dialog } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -18,15 +26,99 @@ import { Models } from "node-appwrite";
 import { actionsDropdownItems } from "@/constants";
 import Link from "next/link";
 import { constructDownloadUrl } from "@/lib/utils";
+import { Input } from "./ui/input";
+import { usePathname } from "next/navigation";
+import { renameFile } from "@/lib/actions/file.actions";
 
 const ActionDropdown = ({ file }: { file: Models.Document }) => {
-  const [isModalOpen, setisModalOpen] = useState(false);
-  const [isDropdownOpen, setisDropdownOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [action, setAction] = useState<ActionType | null>(null);
+  const [name, setName] = useState(file.name);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const path = usePathname();
+
+  const closeAllModals = () => {
+    setIsModalOpen(false);
+    setIsDropdownOpen(false);
+    setAction(null);
+    setName(file.name);
+  };
+
+  const handleActions = async () => {
+    if (!action) return;
+
+    setIsLoading(true);
+    let success = false;
+
+    const actions = {
+      rename: () =>
+        renameFile({
+          fileId: file.$id,
+          name: name,
+          extension: file.extension,
+          path: path,
+        }),
+
+      share: () => console.log("share"),
+      delete: () => console.log("delete"),
+    };
+
+    success = await actions[action.value as keyof typeof actions]();
+
+    if (success) closeAllModals();
+
+    setIsLoading(false);
+  };
+
+  const renderDialogContent = () => {
+    if (!action) return null;
+
+    const { value, label } = action;
+
+    return (
+      <DialogContent className="shad-dialog button">
+        <DialogHeader className="flex flex-col gap-3">
+          <DialogTitle className="text-center text-light-100">
+            {label}
+          </DialogTitle>
+          <DialogDescription>
+            {value === "rename" && (
+              <Input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            )}
+          </DialogDescription>
+        </DialogHeader>
+        {["rename", "delete", "share"].includes(value) && (
+          <DialogFooter className="flex flex-col gap-3 md:flex-row">
+            <Button onClick={closeAllModals} className="modal-cancel-button">
+              Cancel
+            </Button>
+            <Button onClick={handleActions} className="modal-submit-button">
+              <p className="capitalize">{value}</p>
+              {isLoading && (
+                <Image
+                  src="/assets/icons/loader.svg"
+                  alt="loader"
+                  width={24}
+                  height={24}
+                  className="animate-spin"
+                />
+              )}
+            </Button>
+          </DialogFooter>
+        )}
+      </DialogContent>
+    );
+  };
 
   return (
-    <Dialog open={isModalOpen} onOpenChange={setisModalOpen}>
-      <DropdownMenu open={isDropdownOpen} onOpenChange={setisDropdownOpen}>
+    <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+      <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
         <DropdownMenuTrigger className="shad-no-focus">
           <Image
             src="/assets/icons/dots.svg"
@@ -52,7 +144,8 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
                     actionItem.value
                   )
                 ) {
-                  setisModalOpen(true);
+                  setIsDropdownOpen(false);
+                  setIsModalOpen(true);
                 }
               }}
             >
@@ -85,6 +178,8 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
           ))}
         </DropdownMenuContent>
       </DropdownMenu>
+
+      {renderDialogContent()}
     </Dialog>
   );
 };
--- storage-app/lib/actions/file.actions.ts
+++ storage-app/lib/actions/file.actions.ts
@@ -7,6 +7,7 @@ import { ID, Models, Query } from "node-appwrite";
 import { constructFileUrl, getFileType, parseStringify } from "../utils";
 import { revalidatePath } from "next/cache";
 import { getCurrentUser } from "./user.actions";
+import { extname } from "path";
 
 const handleError = (error: unknown, message: string) => {
   console.log(error, message);
@@ -89,5 +90,30 @@ export const getFiles = async () => {
         return parseStringify(files);
     } catch (error) {
         handleError(error, "failed to get files"); 
+    }
+}
+
+export const renameFile = async ({ fileId, name, extension, path}: RenameFileProps) => {
+    const { databases } = await createAdminClient();
+
+    try {
+        const newName = `${name}.${extension}`;
+
+        const updatedFile = await databases.updateDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.filesCollectionId,
+            fileId,
+            {
+                name: newName
+            },
+
+        );
+
+        revalidatePath(path);
+        
+        return parseStringify(updatedFile);
+
+    } catch (error) {
+        handleError(error, "failed to rename file");
     }
 }
