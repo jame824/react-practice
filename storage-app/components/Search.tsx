9c7fe82453a91b422a18b5cfeadbc35a29b5ed63
James Fu <fu.jinghua8@gmail.com>
2026-02-04 13:12:29 -0800

    feat: implemented search component and server action for type based file display

--- storage-app/components/Search.tsx
+++ storage-app/components/Search.tsx
@@ -1,7 +1,109 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import { Input } from "./ui/input";
+import { useState, useEffect } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { getFiles } from "@/lib/actions/file.actions";
+import { Models } from "node-appwrite";
+import Thumbnail from "./Thumbnail";
+import FormattedDateTime from "./FormattedDateTime";
 
 const Search = () => {
-  return <div>Search</div>;
+  const [query, setQuery] = useState("");
+
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("query") || "";
+  const [results, setResults] = useState<Models.Document[]>([]);
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const path = usePathname();
+
+  useEffect(() => {
+    const fetchFiles = async () => {
+      if (!query) {
+        setResults([]);
+        setOpen(false);
+        return router.push(path.replace(searchParams.toString(), ""));
+      }
+      const files = await getFiles({ searchText: query });
+      setResults(files.documents);
+      setOpen(true);
+    };
+
+    fetchFiles();
+  }, [query]);
+
+  useEffect(() => {
+    if (!searchQuery) {
+      setQuery("");
+    }
+  }, [searchQuery]);
+
+  const handleClickItem = (file: Models.Document) => {
+    setOpen(false);
+    setResults([]);
+
+    router.push(
+      `/${
+        file.type === "video" || file.type === "audio"
+          ? "media"
+          : file.type + "s"
+      }?query=${query}`,
+    );
+  };
+
+  return (
+    <div className="search">
+      <div className="search-input-wrapper">
+        <Image
+          src="/assets/icons/search.svg"
+          alt="Search"
+          width={24}
+          height={24}
+        />
+        <Input
+          value={query}
+          placeholder="Search... "
+          className="search-input"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </div>
+
+      {open && (
+        <ul className="search-result">
+          {results.length > 0 ? (
+            results.map((file) => (
+              <li
+                key={file.$id}
+                className="flex items-center justify-between"
+                onClick={() => handleClickItem(file)}
+              >
+                <div className="flex cursor-pointer items-center gap-4">
+                  <Thumbnail
+                    type={file.type}
+                    extension={file.extension}
+                    url={file.url}
+                    className="size-9 min-w-9"
+                  />
+                  <p className="subtitle-2 text-light-100 line-clamp-1">
+                    {file.name}
+                  </p>
+                </div>
+
+                <FormattedDateTime
+                  date={file.$createdAt}
+                  className="caption text-light-200 line-clamp-1"
+                />
+              </li>
+            ))
+          ) : (
+            <p className="empty-result">No files found</p>
+          )}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Search;
--- storage-app/lib/actions/file.actions.ts
+++ storage-app/lib/actions/file.actions.ts
@@ -7,20 +7,28 @@ import { ID, Models, Query } from "node-appwrite";
 import { constructFileUrl, getFileType, parseStringify } from "../utils";
 import { revalidatePath } from "next/cache";
 import { getCurrentUser } from "./user.actions";
-import { extname } from "path";
 
 const handleError = (error: unknown, message: string) => {
   console.log(error, message);
   throw error;
 }
 
-const createQueries = (currentUser: Models.Document) => {
+const createQueries = (currentUser: Models.Document, types: string[], searchText: string, sort: string, limit?: number) => {
     const queries = [
         Query.or([
             Query.equal('owner', [currentUser.$id]),
             Query.contains('users', [currentUser.email])]
         )
     ];
+
+    if(types.length > 0) queries.push(Query.equal("type", types));
+    if(searchText) queries.push(Query.contains("name", searchText));
+    if(limit) queries.push(Query.limit(limit));
+
+    const [sortBy, orderBy] = sort.split("-")
+
+    queries.push(orderBy === 'asc' ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy));
+
     return queries
 }
 
@@ -68,14 +76,14 @@ export const uploadFile = async ({file, ownerId, accountId, path}: UploadFilePro
     }
 };
 
-export const getFiles = async () => {
+export const getFiles = async ({types = [], searchText = '', sort = '$createdAt-desc', limit }: GetFilesProps) => {
     const { databases } = await createAdminClient();
     try {
         const currentUser = await getCurrentUser();
 
         if(!currentUser) throw new Error("User not found");
 
-        const queries = createQueries(currentUser);
+        const queries = createQueries(currentUser, types, searchText, sort, limit);
 
         const files = await databases.listDocuments(
             appwriteConfig.databaseId,
