3224750e0d0dbbf91ad5459f280365efa9513dec
James Fu <fu.jinghua8@gmail.com>
2026-02-15 15:12:56 -0800

    day 2, react warm-up with creating a new detail row with a couple html elements and props, reiterated day 1

--- storage-app/components/ActionsModalContent.practice.tsx
+++ storage-app/components/ActionsModalContent.practice.tsx
@@ -12,3 +12,20 @@ const ImageThumbnail = ({ file }: { file: Models.Document }) => {
     </div>
   </div>;
 };
+
+const DetailRow = ({ label, value }: { label: string; value: string }) => {
+  <div>
+    <p>{label}</p>
+    <p>{value}</p>
+  </div>;
+};
+
+const ImageThumbnail = ({ file }: { file: Models.Document }) => {
+  <div>
+    <Thumbnail type={file.type} extension={file.extension} url={file.url} />
+    <div>
+      <p>{file.name}</p>
+      <FormattedDateTime date={file.$createdAt} />
+    </div>
+  </div>;
+};
