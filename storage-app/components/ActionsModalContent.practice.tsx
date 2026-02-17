e2242028bff5ec3e513d80e81a50e428ce7e902e
James Fu <fu.jinghua8@gmail.com>
2026-02-16 16:02:39 -0800

    day 3, react warm-up, working through components and Props, exporting components and using internal reusauble components, reviewed TSX essential concept: functions, and iterated DetailRow component

--- storage-app/components/ActionsModalContent.practice.tsx
+++ storage-app/components/ActionsModalContent.practice.tsx
@@ -3,29 +3,47 @@ import { Models } from "node-appwrite";
 import Thumbnail from "./Thumbnail";
 import FormattedDateTime from "./FormattedDateTime";
 
-const ImageThumbnail = ({ file }: { file: Models.Document }) => {
+const ImageThumbnail = ({ file }: { file: Models.Document }) => (
   <div>
     <Thumbnail type={file.type} extension={file.extension} url={file.url} />
     <div>
       <p>{file.name}</p>
       <FormattedDateTime date={file.$createdAt} />
     </div>
-  </div>;
-};
+  </div>
+);
 
-const DetailRow = ({ label, value }: { label: string; value: string }) => {
+const DetailRow = ({ label, value }: { label: string; value: string }) => (
   <div>
     <p>{label}</p>
     <p>{value}</p>
-  </div>;
-};
+  </div>
+);
 
-const ImageThumbnail = ({ file }: { file: Models.Document }) => {
+const ImageThumbnail = ({ file }: { file: Models.Document }) => (
   <div>
     <Thumbnail type={file.type} extension={file.extension} url={file.url} />
     <div>
       <p>{file.name}</p>
       <FormattedDateTime date={file.$createdAt} />
     </div>
-  </div>;
+  </div>
+);
+
+export const FileDetails = ({ file }: { file: Models.Document }) => {
+  return (
+    <>
+      <ImageThumbnail file={file} />
+      <div>
+        <DetailRow label="name" value={file.name} />
+      </div>
+    </>
+  );
 };
+
+const DetailRow = ({ label, value }: { label: string; value: string }) => (
+  <div>
+    <p>{label}</p>
+    <p>{value}</p>
+  </div>
+);
