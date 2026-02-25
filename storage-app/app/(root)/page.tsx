d22f1d930ee5a88e7fa292c3e535758302e2a47b
James Fu <fu.jinghua8@gmail.com>
2026-02-25 14:18:51 -0800

    Dashboard from scratch day 1: built a server component from scratch, figured out the data fetching, reverse-engineered the totalSpace shape from reading the code, and got both rendering. Next steps: flesh out the ui, finish with server action for finding real totalspace usage instaed of mock data display.

--- storage-app/app/(root)/page.tsx
+++ storage-app/app/(root)/page.tsx
@@ -1,9 +1,53 @@
-export default function Home() {
+import Card from "@/components/Card";
+import { getFiles } from "@/lib/actions/file.actions";
+import { getUsageSummary } from "@/lib/utils";
+import { Models } from "node-appwrite";
+
+const Dashboard = async () => {
+  const files = await getFiles({ types: [], limit: 10 });
+
+  const mockTotalSpace = {
+    document: {
+      size: 10,
+      latestDate: "2026-10-20",
+    },
+    image: {
+      size: 10,
+      latestDate: "2026-10-20",
+    },
+    video: {
+      size: 10,
+      latestDate: "2026-10-20",
+    },
+    audio: {
+      size: 10,
+      latestDate: "2026-10-20",
+    },
+    other: {
+      size: 10,
+      latestDate: "2026-10-20",
+    },
+  };
+
+  const fileUsage = getUsageSummary(mockTotalSpace);
+
   return (
-    <div className="flex-center h-screen">
-      <h1 className="h1">
-        StoreIt - The only storage solution you need.
-      </h1>
+    <div className="dashboard-container">
+      <div className="dashboard-summary-list">
+        Chart
+        <ul>
+          {fileUsage.map((object) => (
+            <li key={object.title}>{object.icon}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="dashboard-recent-files">
+        {files.documents.map((file: Models.Document) => (
+          <Card key={file.$id} file={file}></Card>
+        ))}
+      </div>
     </div>
   );
-}
+};
+
+export default Dashboard;
