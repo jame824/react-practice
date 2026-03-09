6dddde852941c02adbfce7fb5af07a05848b7926
James Fu <fu.jinghua8@gmail.com>
2026-03-09 11:25:57 -0700

    feat: finalized recent files section styling

--- storage-app/app/(root)/page.tsx
+++ storage-app/app/(root)/page.tsx
@@ -1,5 +1,4 @@
-import Card from "@/components/Card";
-import { getFiles } from "@/lib/actions/file.actions";
+import { getFiles, getTotalSpaceUsed } from "@/lib/actions/file.actions";
 import { convertFileSize, getUsageSummary } from "@/lib/utils";
 import { Models } from "node-appwrite";
 import Link from "next/link";
@@ -7,10 +6,10 @@ import Image from "next/image";
 import FormattedDateTime from "@/components/FormattedDateTime";
 import Chart from "@/components/Chart";
 import { Separator } from "@/components/ui/separator";
+import ActionDropdown from "@/components/ActionDropdown";
+import Thumbnail from "@/components/Thumbnail";
 
 const Dashboard = async () => {
-  const files = await getFiles({ types: [], limit: 10 });
-
   const mockTotalSpace = {
     document: {
       size: 10,
@@ -34,12 +33,17 @@ const Dashboard = async () => {
     },
   };
 
-  const usageSummary = getUsageSummary(mockTotalSpace);
+  const [files, totalSpace] = await Promise.all([
+    getFiles({ types: [], limit: 10 }),
+    getTotalSpaceUsed(),
+  ]);
+
+  const usageSummary = getUsageSummary(totalSpace);
 
   return (
     <div className="dashboard-container">
       <div>
-        <Chart />
+        <Chart used={totalSpace.used} />
         <ul className="dashboard-summary-list">
           {usageSummary.map((summary) => (
             <Link
@@ -72,11 +76,40 @@ const Dashboard = async () => {
           ))}
         </ul>
       </div>
-      <div className="dashboard-recent-files">
-        {files.documents.map((file: Models.Document) => (
-          <Card key={file.$id} file={file}></Card>
-        ))}
-      </div>
+      <section className="dashboard-recent-files">
+        <h2 className="h3 xl:h2 text-light-100">Recent files uploaded</h2>
+        {files.documents.length > 0 ? (
+          <ul className="mt-5 flex flex-col gap-5">
+            {files.documents.map((file: Models.Document) => (
+              <Link
+                href={file.url}
+                target="_blank"
+                className="flex items-center gap-3"
+                key={file.$id}
+              >
+                <Thumbnail
+                  type={file.type}
+                  extension={file.extension}
+                  url={file.url}
+                />
+
+                <div className="recent-file-details">
+                  <div className="flex flex-col gap-1">
+                    <p className="recent-file-name">{file.name}</p>
+                    <FormattedDateTime
+                      date={file.$createdAt}
+                      className="caption"
+                    />
+                  </div>
+                  <ActionDropdown file={file} />
+                </div>
+              </Link>
+            ))}
+          </ul>
+        ) : (
+          <p className="empty-list">No files uploaded</p>
+        )}
+      </section>
     </div>
   );
 };
